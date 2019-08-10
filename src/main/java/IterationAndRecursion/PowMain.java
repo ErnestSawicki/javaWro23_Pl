e054e1756c8732c6d64d9fc0f7b3192d05bd9b00
Ernest Sawicki <ErnestSawicki94@gmail.com>
2019-08-10 12:31:15 +0200

    IterationAndRecursion

---
+++ src/main/java/IterationAndRecursion/PowMain.java
@@ -0,0 +1,10 @@
+package IterationAndRecursion;
+
+public class PowMain {
+    public static void main(String[] args) {
+        TwoToPower two = new TwoToPower();
+
+        System.out.println(two.toPowerOfTwo(4));
+        System.out.println(two.powRecursion(4));
+    }
+}
---
+++ src/main/java/IterationAndRecursion/TwoToPower.java
@@ -0,0 +1,19 @@
+package IterationAndRecursion;
+
+public class TwoToPower {
+    public int toPowerOfTwo(int power){
+        int TwoToPower = 1;
+        while (power > 0){
+            TwoToPower *= 2;
+            power--;
+        }
+        return TwoToPower;
+    }
+
+    public int powRecursion(int power){
+        if (power ==0)
+            return 1;
+        return powRecursion(power -1) *2;
+    }
+
+}
