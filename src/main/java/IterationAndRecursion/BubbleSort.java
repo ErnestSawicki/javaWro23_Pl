45611100f8640a6beffb737820b8f45edc45d9dc
Ernest Sawicki <ErnestSawicki94@gmail.com>
2019-08-10 15:43:28 +0200

    bubble sort

--- src/main/java/IterationAndRecursion/BubbleSort.java
+++ src/main/java/IterationAndRecursion/BubbleSort.java
@@ -3,15 +3,19 @@ package IterationAndRecursion;
 public class BubbleSort {
 
     public int[] bubbleSort(int[] arr) {
-        for (int j = 0; j < arr.length - 1; j++) {
+        boolean isSorted = false;
+        while (isSorted == false) {
+            isSorted = true;
             for (int i = 0; i < arr.length - 1; i++) {
                 if (arr[i] > arr[i + 1]) {
                     int tmp = arr[i + 1];
                     arr[i + 1] = arr[i];
                     arr[i] = tmp;
+                    isSorted = false;
                 }
             }
         }
+
         return arr;
     }
 }
--- src/main/java/IterationAndRecursion/BubbleSortMain.java
+++ src/main/java/IterationAndRecursion/BubbleSortMain.java
@@ -3,6 +3,12 @@ package IterationAndRecursion;
 public class BubbleSortMain {
     public static void main(String[] args) {
         int[] arr = new int[]{4,2,5,1,7};
+        System.out.print("{");
+        for (int i = 0; i < arr.length - 1; i++){
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.print(arr[arr.length -1] + "}");
+        System.out.println();
 
         BubbleSort sort = new BubbleSort();
         sort.bubbleSort(arr);
